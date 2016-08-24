b3e5039e0ecaf3b690057c2fc68effaac170c5f4
Steven Alexander <steven.william.alexander@googlemail.com>
2016-08-24 14:11:15 +0100

    added mongo

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,16 +1,67 @@
 var express = require('express')
 var router = express.Router()
 
+var mongoClient = require('mongodb').MongoClient
+
+var url = 'mongodb://mongo:27017/app'
+
+var findValues = function (db, callback) {
+  var collection = db.collection('values')
+
+  collection.find({}).toArray(function (err, docs) {
+    if (err) {
+      console.log('Err: ' + err)
+    }
+    console.dir(docs)
+    callback(docs)
+  })
+}
+
+var addValue = function (db, value, callback) {
+  var collection = db.collection('values')
+
+  collection.insertOne({ test: value }, function (err, result) {
+    if (err) {
+      console.log('Err: ' + err)
+    }
+    callback(result)
+  })
+}
+
 /* GET home page. */
 router.get('/', function (req, res) {
   var values = []
-  res.render('index', {title: 'Node Redis', values: values})
+
+  mongoClient.connect(url, function (err, db) {
+    if (err) {
+      console.log('Err: ' + err)
+    } else {
+      console.log('Connected correctly to server')
+      findValues(db, function (docs) {
+        for (var i = 0; i < docs.length; i++) {
+          values.push(docs[i].test)
+        }
+        console.log('Found values: ' + values)
+        res.render('index', {title: 'Node Redis', values: values})
+      })
+    }
+  })
 })
 
 router.post('/redis', function (req, res) {
   var test = req.body.test
 
   console.log('test: ' + test)
+
+  mongoClient.connect(url, function (err, db) {
+    if (err) {
+      console.log('Err: ' + err)
+    } else {
+      addValue(db, test, function () {
+        console.log('Inserted value: ' + test)
+      })
+    }
+  })
 
   res.redirect('/')
 })
